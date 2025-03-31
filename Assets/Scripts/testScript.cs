27629b42e612e24da9d9e05c621e44af6539825c
Pavle Miljkovic <miljkovicpavle1@gmail.com>
2025-03-31 17:35:52 +0200

    Initial commit

---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("This is a test for unity");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
